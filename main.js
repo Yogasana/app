ec993dda55fe1de94dad07aaa496a2e1af0e7344
Yogasana <143020017+Yogasana@users.noreply.github.com>
2023-10-06 22:25:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -133,50 +133,22 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("custom-video-h").load();
     });
 });
-// Check if the browser supports service workers
-if ('serviceWorker' in navigator) {
-  // Listen for the "load" event to ensure all resources are loaded
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch((error) => {
-        console.error('Service Worker registration failed:', error);
-      });
-  });
+// Get the night mode toggle button
+const nightModeToggle = document.getElementById('night-mode-toggle');
 
-  // Listen for the "beforeinstallprompt" event to prompt for installation
-  window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent the browser's default installation prompt
-    e.preventDefault();
-
-    // Store the event for later use
-    deferredInstallPrompt = e;
-
-    // Show a custom install button or banner
-    // For example, you can display a button with an "Install App" label
-    installButton.style.display = 'block';
-
-    // Attach an event listener to the custom install button
-    installButton.addEventListener('click', () => {
-      // Show the browser's installation prompt when the custom button is clicked
-      deferredInstallPrompt.prompt();
-
-      // Wait for the user to respond to the prompt
-      deferredInstallPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the PWA installation');
-        } else {
-          console.log('User declined the PWA installation');
-        }
-
-        // Reset the deferred prompt variable
-        deferredInstallPrompt = null;
-
-        // Hide the custom install button
-        installButton.style.display = 'none';
-      });
-    });
-  });
-}
+// Listen for a click event on the button
+nightModeToggle.addEventListener('click', () => {
+  // Toggle the 'night-mode' class on the body element
+  document.body.classList.toggle('night-mode');
+  document.getElementById("cc");
+  // Update the button text and class
+  if (document.body.classList.contains('night-mode')) {
+    nightModeToggle.innerHTML = '<i class="bx bx-sun"></i> Day Mode';
+    nightModeToggle.classList.remove('night-mode-off');
+    nightModeToggle.classList.add('night-mode-on');
+  } else {
+    nightModeToggle.innerHTML = '<i class="bx bx-moon"></i> Night Mode';
+    nightModeToggle.classList.remove('night-mode-on');
+    nightModeToggle.classList.add('night-mode-off');
+  }
+});
